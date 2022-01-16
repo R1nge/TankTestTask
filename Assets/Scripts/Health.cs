fd70a970c6ab70ef9f1479c33db970cbbc08ca89
R1nge <r1ngegamedev@gmail.com>
2022-01-16 09:53:17 +0300

    Refactored Health

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,16 +3,21 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    [SerializeField] private float health;
-    [SerializeField] private float takenDamageMultiplier;
-    public event Action OnDieEvent;
+	[SerializeField] private float health, maxHealth;
+	[SerializeField] private float takenDamageMultiplier;
+	public event Action OnDieEvent;
 
-    public void TakeDamage(float amount)
-    {
-        health -= amount * takenDamageMultiplier;
-        if (health <= 0)
-        {
-            OnDieEvent?.Invoke();
-        }
-    }
+	private void Start ()
+	{
+		health = maxHealth;
+	}
+
+	public void TakeDamage (float amount)
+	{
+		health -= amount*takenDamageMultiplier;
+		if (health <= 0)
+		{
+			OnDieEvent?.Invoke();
+		}
+	}
 }
