0f1ef0276e0b30a3ccfe46cca56a27d63cc5ac2a
R1nge <r1ngegamedev@gmail.com>
2022-01-16 07:26:37 +0300

    Refactored Enemy

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -1,36 +1,39 @@
 ﻿using Pathfinding;
 using UnityEngine;
-
 public abstract class Enemy : MonoBehaviour
 {
-    public float speed;
-    public float damage;
-    public AIDestinationSetter aiDestinationSetter;
-    public AIPath aiPath;
-    private Health _health;
+	public float speed;
+	public float damage;
+	public AIDestinationSetter aiDestinationSetter;
+	public AIPath aiPath;
+	private Health _health;
 
-    private void Awake()
-    {
-        aiDestinationSetter = GetComponent<AIDestinationSetter>();
-        aiPath = GetComponent<AIPath>();
-        _health = GetComponent<Health>();
-        _health.OnDieEvent += OnDeath;
+	private void Awake ()
+	{
+		aiDestinationSetter = GetComponent<AIDestinationSetter>();
+		aiPath = GetComponent<AIPath>();
+		_health = GetComponent<Health>();
+		_health.OnDieEvent += OnDeath;
 
-        aiPath.maxSpeed = speed;
-    }
+		aiPath.maxSpeed = speed;
+	}
 
-    protected virtual void OnDeath()
-    {
-        Destroy(gameObject);
-    }
+	protected virtual void OnDeath ()
+	{
+		Destroy(gameObject);
+	}
 
-    private void OnCollisionStay2D(Collision2D other)
-    {
-        if (other.transform.CompareTag("Player"))
-        {
-            other.transform.TryGetComponent(out Health health);
-            health.TakeDamage(damage);
-            Destroy(gameObject);
-        }
-    }
+	private void OnCollisionStay2D (Collision2D other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			other.transform.TryGetComponent(out Health health);
+			health.TakeDamage(damage);
+		}
+	}
+
+	private void OnDestroy ()
+	{
+		_health.OnDieEvent -= OnDeath;
+	}
 }
