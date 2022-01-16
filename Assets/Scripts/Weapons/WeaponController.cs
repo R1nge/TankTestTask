7a16f67bff77c49cd6bea66fa720342e87625541
R1nge <r1ngegamedev@gmail.com>
2022-01-16 07:24:03 +0300

    Added Weapon Controller

---
+++ Assets/Scripts/Weapons/WeaponController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+public class WeaponController : MonoBehaviour
+{
+	[SerializeField] private Weapon[] weapons;
+	private Weapon _currentWeapon;
+	private int _weaponIndex;
+	private PlayerInput _playerInput;
+
+	private void Awake ()
+	{
+		_playerInput = GetComponent<PlayerInput>();
+		_playerInput.ShootEvent += Shoot;
+		_playerInput.SwapWeaponEvent += SwapWeapons;
+	}
+
+	private void Start ()
+	{
+		_currentWeapon = weapons[0];
+		_currentWeapon.gameObject.SetActive(true);
+	}
+
+	private void Shoot ()
+	{
+		_currentWeapon.Shoot(_currentWeapon.damage);
+	}
+
+	private void SwapWeapons (int index)
+	{
+		if (_weaponIndex < weapons.Length - 1)
+		{
+			_currentWeapon.gameObject.SetActive(false);
+			_weaponIndex++;
+			_currentWeapon = weapons[_weaponIndex];
+			_currentWeapon.gameObject.SetActive(true);
+		}
+
+		if (_weaponIndex > 0)
+		{
+			_currentWeapon.gameObject.SetActive(false);
+			_weaponIndex--;
+			_currentWeapon = weapons[_weaponIndex];
+			_currentWeapon.gameObject.SetActive(true);
+		}
+	}
+
+	private void OnDestroy ()
+	{
+		_playerInput.ShootEvent -= Shoot;
+		_playerInput.SwapWeaponEvent -= SwapWeapons;
+	}
+}
