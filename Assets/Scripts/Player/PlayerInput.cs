cd86de6001b2764ec194794a3d568a088108417a
R1nge <r1ngegamedev@gmail.com>
2022-01-16 07:25:50 +0300

    Refactored Player Input,Player Movement

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,27 +1,29 @@
 using System;
 using UnityEngine;
-
 public class PlayerInput : MonoBehaviour
 {
-    public event Action OnMoveForwardEvent;
-    public event Action OnRotateLeftEvent;
-    public event Action OnRotateRightEvent;
+	public bool MoveForward { get; private set; }
+	public bool RotateLeft { get; private set; }
+	public bool RotateRight { get; private set; }
 
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            OnMoveForwardEvent?.Invoke();
-        }
+	public event Action ShootEvent = delegate {};
+	public event Action<int> SwapWeaponEvent = delegate {};
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            OnRotateLeftEvent?.Invoke();
-        }
+	private void Update ()
+	{
+		MoveForward = Input.GetKey(KeyCode.UpArrow);
 
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            OnRotateRightEvent?.Invoke();
-        }
-    }
+		RotateLeft = Input.GetKey(KeyCode.LeftArrow);
+
+		RotateRight = Input.GetKey(KeyCode.RightArrow);
+
+		if (Input.GetButtonDown("Fire1"))
+			ShootEvent?.Invoke();
+
+		if (Input.GetKeyDown(KeyCode.Q))
+			SwapWeaponEvent?.Invoke(1);
+
+		if (Input.GetKeyDown(KeyCode.W))
+			SwapWeaponEvent?.Invoke(-1);
+	}
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,32 +1,45 @@
 using UnityEngine;
-
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private int speed;
-    [SerializeField] private int rotationSpeed;
-    private PlayerInput _playerInput;
+	[SerializeField] private int speed;
+	[SerializeField] private int rotationSpeed;
+	private PlayerInput _playerInput;
 
-    private void Awake()
-    {
-        _playerInput = GetComponent<PlayerInput>();
+	private void Awake ()
+	{
+		_playerInput = GetComponent<PlayerInput>();
+	}
 
-        _playerInput.OnMoveForwardEvent += MoveForward;
-        _playerInput.OnRotateLeftEvent += RotateLeft;
-        _playerInput.OnRotateRightEvent += RotateRight;
-    }
+	private void Update ()
+	{
+		if (_playerInput.MoveForward)
+		{
+			MoveForward();
+		}
 
-    private void MoveForward()
-    {
-        transform.position += transform.up * speed * Time.deltaTime;
-    }
+		if (_playerInput.RotateLeft)
+		{
+			RotateLeft();
+		}
 
-    private void RotateLeft()
-    {
-        transform.Rotate(new Vector3(0, 0, rotationSpeed * Time.deltaTime));
-    }
+		if (_playerInput.RotateRight)
+		{
+			RotateRight();
+		}
+	}
 
-    private void RotateRight()
-    {
-        transform.Rotate(new Vector3(0, 0, -rotationSpeed * Time.deltaTime));
-    }
+	private void MoveForward ()
+	{
+		transform.position += transform.up*speed*Time.deltaTime;
+	}
+
+	private void RotateLeft ()
+	{
+		transform.Rotate(new Vector3(0, 0, rotationSpeed*Time.deltaTime));
+	}
+
+	private void RotateRight ()
+	{
+		transform.Rotate(new Vector3(0, 0, -rotationSpeed*Time.deltaTime));
+	}
 }
