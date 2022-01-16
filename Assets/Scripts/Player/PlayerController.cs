5a1f859a823affc247c619b643614e69c12ca3f4
R1nge <r1ngegamedev@gmail.com>
2022-01-16 07:25:17 +0300

    Refactored PlayerController

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,55 +1,23 @@
 using Unity.Mathematics;
 using UnityEngine;
-
 public class PlayerController : MonoBehaviour
 {
-    [Header("Weapons")]
-    [SerializeField] private Weapon currentWeapon;
-    [SerializeField] private Weapon[] weapons;
-    private int _weaponIndex;
-    
-    private void Start()
-    {
-        currentWeapon = weapons[0];
-        currentWeapon.gameObject.SetActive(true);
-    }
+	private Health _health;
 
-    private void Update()
-    {
-        Shoot();
-        SwapWeapons();
-    }
+	private void Awake ()
+	{
+		_health = GetComponent<Health>();
+		_health.OnDieEvent += Respawn;
+	}
 
-    private void Shoot()
-    {
-        if (Input.GetKeyDown(KeyCode.X))
-        {
-            currentWeapon.Shoot(currentWeapon.damage);
-        }
-    }
+	private void Respawn ()
+	{
+		Instantiate(gameObject, new Vector3(0, 0, 0), quaternion.identity);
+		Destroy(gameObject);
+	}
 
-    private void SwapWeapons()
-    {
-        if (Input.GetKeyDown(KeyCode.Q) && _weaponIndex < weapons.Length - 1)
-        {
-            currentWeapon.gameObject.SetActive(false);
-            _weaponIndex++;
-            currentWeapon = weapons[_weaponIndex];
-            currentWeapon.gameObject.SetActive(true);
-        }
-
-        if (Input.GetKeyDown(KeyCode.W) && _weaponIndex > 0)
-        {
-            currentWeapon.gameObject.SetActive(false);
-            _weaponIndex--;
-            currentWeapon = weapons[_weaponIndex];
-            currentWeapon.gameObject.SetActive(true);
-        }
-    }
-    
-    private void Respawn()
-    {
-        Instantiate(gameObject, new Vector3(0, 0, 0), quaternion.identity);
-        Destroy(gameObject);
-    }
+	private void OnDestroy ()
+	{
+		_health.OnDieEvent -= Respawn;
+	}
 }
