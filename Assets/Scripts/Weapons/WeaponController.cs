e0a8051028deb7395eb1465ba92b41a0d8bc3af2
R1nge <r1ngegamedev@gmail.com>
2022-02-17 10:01:46 +0300

    Fixed Weapon Swap

--- Assets/Scripts/Weapons/WeaponController.cs
+++ Assets/Scripts/Weapons/WeaponController.cs
@@ -1,51 +1,44 @@
 using UnityEngine;
+
 public class WeaponController : MonoBehaviour
 {
-	[SerializeField] private Weapon[] weapons;
-	private Weapon _currentWeapon;
-	private int _weaponIndex;
-	private PlayerInput _playerInput;
+    [SerializeField] private Weapon[] weapons;
+    private Weapon _currentWeapon;
+    private int _weaponIndex;
+    private PlayerInput _playerInput;
 
-	private void Awake ()
-	{
-		_playerInput = GetComponent<PlayerInput>();
-		_playerInput.ShootEvent += Shoot;
-		_playerInput.SwapWeaponEvent += SwapWeapons;
-	}
+    private void Awake()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+        _playerInput.ShootEvent += Shoot;
+        _playerInput.SwapWeaponEvent += SwapWeapons;
+    }
 
-	private void Start ()
-	{
-		_currentWeapon = weapons[0];
-		_currentWeapon.gameObject.SetActive(true);
-	}
+    private void Start()
+    {
+        _currentWeapon = weapons[0];
+        _currentWeapon.gameObject.SetActive(true);
+    }
 
-	private void Shoot ()
-	{
-		_currentWeapon.Shoot(_currentWeapon.damage);
-	}
+    private void Shoot()
+    {
+        _currentWeapon.Shoot(_currentWeapon.damage);
+    }
 
-	private void SwapWeapons (int index)
-	{
-		if (_weaponIndex < weapons.Length - 1)
-		{
-			_currentWeapon.gameObject.SetActive(false);
-			_weaponIndex++;
-			_currentWeapon = weapons[_weaponIndex];
-			_currentWeapon.gameObject.SetActive(true);
-		}
+    private void SwapWeapons(int index)
+    {
+        if (_weaponIndex + index >= 0 && _weaponIndex + index <= weapons.Length - 1)
+        {
+            _currentWeapon.gameObject.SetActive(false);
+            _weaponIndex += index;
+            _currentWeapon = weapons[_weaponIndex];
+            _currentWeapon.gameObject.SetActive(true);
+        }
+    }
 
-		if (_weaponIndex > 0)
-		{
-			_currentWeapon.gameObject.SetActive(false);
-			_weaponIndex--;
-			_currentWeapon = weapons[_weaponIndex];
-			_currentWeapon.gameObject.SetActive(true);
-		}
-	}
-
-	private void OnDestroy ()
-	{
-		_playerInput.ShootEvent -= Shoot;
-		_playerInput.SwapWeaponEvent -= SwapWeapons;
-	}
+    private void OnDestroy()
+    {
+        _playerInput.ShootEvent -= Shoot;
+        _playerInput.SwapWeaponEvent -= SwapWeapons;
+    }
 }
